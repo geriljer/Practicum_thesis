1e755c196fc154cc00cf42aceea51904343411ec
Alexandr Levashov <geriljer@gmail.com>
2024-07-25 11:48:24 +0300

    edited src/views/Profile.vue to resolve an issue with Remove this deprecated Center element L2

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -1,5 +1,5 @@
 <template>
-  <Center class="h-100">
+  <div class="container d-flex justify-content-center align-items-center h-100">
 
     <div class="card d-inline-block text-start">
 
@@ -21,7 +21,7 @@
 
     </div>
 
-  </Center>
+  </div>
 </template>
 
 <script lang="ts">
